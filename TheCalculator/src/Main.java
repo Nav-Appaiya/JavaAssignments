b493a7563501eb588075be887c321e32d6d0ab8f
Nav Appaiya <navarajh@gmail.com>
2015-03-30 19:55:44 +0200

    Update calculator

---
+++ TheCalculator/src/Main.java
@@ -0,0 +1,12 @@
+/**
+ * Created by Nav on 23-3-2015.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        Calculator c = new Calculator();
+        c.setVisible(true);
+    }
+
+
+}

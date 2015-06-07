130ff5b85db9442e00328188f9e2f42678d762ed
Nav Appaiya <navarajh@gmail.com>
2015-06-08 00:24:15 +0200

    Added some explaination on the EventQueue invokeLater method

--- Rekenmachine/src/main/Main.java
+++ Rekenmachine/src/main/Main.java
@@ -1,13 +1,22 @@
 package main;
 
 /**
- * Created by admin on 7-6-2015.
+ * Created & Updated by Nav Appaiya on 7-6-2015.
  */
 import java.awt.EventQueue;
 
 public class Main {
 
     public static void main(String[] args) {
+        /**
+         * Nav Appaiya updated June 8
+         *
+         * "Causes runnable to have its run method called in the
+         * dispatch thread of the system EventQueue.
+         * This will happen after all pending events are processed."
+         *
+         * Reference http://docs.oracle.com/javase/7/docs/api/java/awt/EventQueue.html
+         */
         EventQueue.invokeLater(new Runnable() {
             public void run() {
                 HoofdScherm hoofdScherm = new HoofdScherm();
